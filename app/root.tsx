2b823b9bd6307d71792880e4e6801fe93d775900
Hiroshi Muto <mhi6o461o@gmail.com>
2024-11-16 15:52:26 +0900

    fix: restore search form and new button

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,22 @@ export default function App() {
       </head>
       <body>
         <div id="sidebar">
-          {/* その他の要素 */}
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
           <nav>
             {contacts.length ? (
               <ul>
