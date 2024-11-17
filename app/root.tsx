5ffe1dfc1e1400743cf9742f34b108f9ba192a4e
Hiroshi Muto <mhi6o461o@gmail.com>
2024-11-17 11:39:34 +0900

    update: add loading state

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import { createEmptyContact, getContacts } from "./data";
 import type { LinksFunction } from "@remix-run/node";
@@ -30,6 +31,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
   return (
     <html lang="en">
       <head>
@@ -86,7 +88,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          id="detail"
+          className={navigation.state === "loading" ? "loading" : ""}
+        >
           <Outlet />
         </div>
 
