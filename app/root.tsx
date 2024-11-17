1f3d795ef5de9cd0723f84d059396fc2c7261590
Hiroshi Muto <mhi6o461o@gmail.com>
2024-11-17 14:47:25 +0900

    feat: add search function

--- app/root.tsx
+++ app/root.tsx
@@ -9,11 +9,13 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 import { createEmptyContact, getContacts } from "./data";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 import appStylesHref from "./app.css?url";
+import { useEffect, useState } from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -24,14 +26,28 @@ export const action = async () => {
   return redirect(`/contacts/${contact.id}/edit`);
 };
 
-export const loader = async () => {
-  const contacts = await getContacts();
-  return { contacts };
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const [query, setQuery] = useState(q || "");
+  const submit = useSubmit();
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  // 戻る/進むボタンをクリックしたときにクエリを
+  // コンポーネントステートと同期させる `useEffect` があります
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q]);
   return (
     <html lang="en">
       <head>
@@ -44,14 +60,27 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
               <input
-                id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                value={query}
+                id="q"
+                name="q"
                 placeholder="Search"
                 type="search"
-                name="q"
+                onChange={(event) => setQuery(event.currentTarget.value)}
               />
+              <div aria-hidden hidden={!searching} id="search-spinner" />
               <div id="search-spinner" aria-hidden hidden={true} />
             </Form>
             <Form method="post">
@@ -90,7 +119,9 @@ export default function App() {
         </div>
         <div
           id="detail"
-          className={navigation.state === "loading" ? "loading" : ""}
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
         >
           <Outlet />
         </div>
